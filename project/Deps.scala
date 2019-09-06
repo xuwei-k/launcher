b952a6ae9c234726c454d5cd5a92e7a1641ec26c
xuwei-k <6b656e6a69@gmail.com>
2019-09-06 04:33:30 +0000

    Update dependencies
    `"junit" % "junit" % "4.11" => "4.12"` found 27 binary incompatibilities
    
    `"net.sf.proguard" % "proguard-base" % "4.8" => "4.11"` found 42 binary incompatibilities

--- project/Deps.scala
+++ project/Deps.scala
@@ -7,11 +7,11 @@ object Deps {
   def lib(m: ModuleID) = libraryDependencies += m
   lazy val sbtIo = Def.setting { "org.scala-sbt" % "io" % sbtVersion.value }
   lazy val sbtCompileInterface = Def.setting("org.scala-sbt" % "interface" % sbtVersion.value)
-  lazy val scalacheck = "org.scalacheck" %% "scalacheck" % "1.11.4"
-  lazy val specs2 = "org.specs2" %% "specs2" % "2.3.11"
-  lazy val junit = "junit" % "junit" % "4.11"
+  lazy val scalacheck = "org.scalacheck" %% "scalacheck" % "1.11.6"
+  lazy val specs2 = "org.specs2" %% "specs2" % "2.3.13"
+  lazy val junit = "junit" % "junit" % "4.12"
 
   // TODO - these should be like the above, just ModuleIDs
-  lazy val ivy = "org.scala-sbt.ivy" % "ivy" % "2.3.0-sbt-b18f59ea3bc914a297bb6f1a4f7fb0ace399e310"
+  lazy val ivy = "org.scala-sbt.ivy" % "ivy" % "2.3.0-sbt-fccfbd44c9f64523b61398a0155784dcbaeae28f"
   lazy val scalaCompiler = Def.setting("org.scala-lang" % "scala-compiler" % scalaVersion.value)
 }
